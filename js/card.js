6f259b5e47b7b90551cd371bab62f4653ce1d195
chiiia12 <llnsbchsep12@gmail.com>
2015-07-28 23:12:00 +0900

    minimum functin

--- js/card.js
+++ js/card.js
@@ -1,57 +1,19 @@
-var cardNum = 3;
+var cardNum;
 
-function Card(index) {
-    var card = $("<div>")
-    card.addClass("cardBox");
-    
-    var backBtn = $("<div>");
-    backBtn.addClass("backBtn-area");
-    
-    var a = $("<a>");
-    var span =$("<span>");
-    span.addClass("txt");
-    a.append(span);
-    backBtn.append(a);
-    card.append(backBtn);
-    
-    var divmv = $("<div>");
-    divmv.addClass("mainVisual-area mainVisual");
-    var spnmv = $("<span>");
-    spnmv.addClass("stock-btn");
-    var img = $("<img>");
-    img.attr("src","img/011100130.jpg");
-    
-    var divol = $("<div>");
-    divol.addClass("overlay");
-    divol.text("レシピタイトル");
-    
-    divmv.append(spnmv);
-    divmv.append(img);
-    divmv.append(divol);
-    card.append(divmv);
-    
-    var lbl = $("<div>");
-    lbl.addClass("label-area");
-    var spnlbl = $("<span>");
-    spnlbl.addClass("label")
-    spnlbl.text("日本酒");
-    lbl.append(spnlbl);
-    card.append(lbl);
-    
-    
-    
-    
-    
+
+function Card(index,element) {
+    var card= $(element);
+    this.widthValue =  200;
+    this.heightValue = 300;
+    this.marginValue = 50;
     this.render = function (scrollY) {
+        console.log(scrollY)
         var offset = scrollY + index * 200;
         card.css({
-            "top": Math.sin(offset / 540 * Math.PI) * 400 + 50 + "px",
+            "top": Math.sin(offset / 540 * Math.PI) * (this.heightValue+this.marginValue) + 50 + "px",
             "left": Math.cos(offset / 540 * Math.PI) * 350 - 340 + "px",
             "transform": "rotateZ(" + offset / (6) + "deg)",
             "z-index": cardNum - index
-        });
-        $(".bg").css({
-            "background-position": scrollY / 5 + "px " + scrollY * (-1) / 2 + "px"
         });
     }
     this.getCard = function () {
@@ -64,16 +26,23 @@ function main() {
     var isDetail = false;
     var currentDetail;
     var cards=[];
-    for (i = 0; i < cardNum; i++) {
-        cards[i] = new Card(i);
-        cards[i].render(scrollY);
-        anchor.append(cards[i].getCard());
-    }
     var intervalId = setInterval(function () {
         render(scrollY);
     }, 30);
 
-
+    
+    console.log($("#box>.card").length);
+    cards = $("#box > .card");
+    cardNum = cards.length;
+    console.log(cards);
+    console.log(cards.length)
+    console.log(cards[0]);
+    $("#box").text("");
+    for(i=0;i<cards.length;i++){
+        cards[i] = new Card(i,cards[i]);
+        cards[i].render(scrollY);
+        anchor.append(cards[i].getCard());
+    }
 
 
     function render(scrollY) {
@@ -99,106 +68,8 @@ function main() {
         lastY = pageY;
 
     });
-
-
-
-
-    //レシピ詳細画面にいくときの挙動
-    $(".cardBox").on("click", function (e) {
-
-
-        currentDetail = this;
-        if (isDetail) {
-            return;
-        }
-        $(this).css({
-            zIndex: 3
-        })
-        $(".nav-category").addClass("opc0");
-
-        clearInterval(intervalId);
-        isDetail = true;
-
-        $(this).addClass("detailPage");
-        $(".cardFrame").addClass("detailPage")
-        var angle = getRotate($(this).css("transform")),
-            top = parseInt($(this).css("top")),
-            left = parseInt($(this).css("left")),
-            height = parseInt($(this).find(" .content-area").css("height")) + 200,
-            diffHeight = height - 350;
-
-        var count = 0;
-        $(this).animate({
-            zIndex: 4
-        }, {
-            duration: 700,
-            step: function (now) {
-                $(this).css({
-                    transform: 'rotateZ(' + angle * (4 - now) + 'deg)',
-                    top: top * (4 - now) + 'px',
-                    left: left * (4 - now) + 'px'
-                        // height:350+diffHeight*(now-3)+"px"
-                });
-            },
-            complete: function (now) {
-                $(".nav-category").addClass("none");
-                $(".backBtn-area").removeClass("none");
-                $(this).animate({
-                    height: height + "px"
-                }, 700);
-
-                $(this).find(".content-area").removeClass("showPage");
-            }
-
-        });
-//        var nextPage = "detail0.html";
-        history.pushState(null, null, "detail");
-        addEventListener("popstate",function(e){
-//            changeContents(location.pathname);
-            history.pushState(null,null,"show.html");
-        },false);
-        
-        //戻るボタンを押したときの挙動
-        $(".backBtn-area").click(function (e) {
-            console.log(scrollY);
-            $(this).addClass("none");
-            isDetail = false;
-
-            console.log(currentDetail);
-            $(currentDetail).removeClass("detailPage");
-            $(".cardFrame").removeClass("detailPage");
-            $(".nav-category").removeClass("opc0");
-            $(currentDetail).animate({
-                height: 350 + "px"
-            }, 700);
-            //animation
-            $(currentDetail).animate({
-                zIndex: 3
-            }, {
-                duration: 700,
-                step: function (now) {
-                    $(currentDetail).css({
-                        transform: 'rotateZ(' + angle * (4 - now) + 'deg)',
-                        top: top * (4 - now) + 'px',
-                        left: left * (4 - now) + 'px'
-                            // height:350+diffHeight*(now-3)+"px"
-                    });
-                },
-                complete: function (now) {
-                    $(".nav-category").removeClass("none");
-                    $(".backBtn-area").addClass("none");
-
-                    $(currentDetail).find(".content-area").addClass("showPage");
-                    intervalId = setInterval(function () {
-                        render(scrollY);
-                    }, 30);
-                }
-            });
-        });
-    });
-
-
 }
+
 main();
 
 function getRotate(t) {
@@ -211,12 +82,3 @@ function getRotate(t) {
     return angle;
 }
 
-function addStocked(el) {
-    var target = $(el).attr("class");
-    if (target == "stock-btn") {
-        $(el).addClass("stocked");
-    } else {
-        $(el).removeClass("stocked");
-    }
-
-}
