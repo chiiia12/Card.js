15a3c211e1cffb3febdb00e2f1bdaff4a6aff0dd
chiiia12 <llnsbchsep12@gmail.com>
2015-08-02 17:10:17 +0900

    delete unnecessarly files

--- js/detail.js
+++
@@ -1,9 +0,0 @@
-function addStocked(el) {
-  var target = $(el).attr("class");
-  if (target == "stock-btn") {
-    $(el).addClass("stocked");
-  } else {
-    $(el).removeClass("stocked");
-  }
-
-}
--- js/mypage.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-   $(".history-thumb .fa-thumbs-o-up").click(function(){
-       $(this).parent("a").attr("href","javascript:void(0)");
-      $(this).addClass("like"); 
-       $(this).parent("a").attr("href","msg.html");
-   });
-});
